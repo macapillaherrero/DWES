cf452e4a182244c1b7aee8e67d9fcef646640c79
macapillaherrero <163423673+macapillaherrero@users.noreply.github.com>
2025-10-19 13:47:33 +0200

    modificaciones en el tema

--- UD1Instalacion/mi-proyecto/app/objetos/ejemplo-objetos.php
+++ UD1Instalacion/mi-proyecto/app/objetos/ejemplo-objetos.php
@@ -25,6 +25,25 @@ class Figura {
     }
 }
 
+class Circulo extends Figura {
+    private $radio;
+
+    public function __construct($color, $radio) {
+        parent::__construct($color, 0); // Un círculo no tiene lados
+        $this->radio = $radio;
+    }
+
+    public function __toString() {
+        return "Círculo de color " . $this->color . " con radio $this->radio.";
+    }
+}
+
+$circulo = new Circulo("verde", 5);
+echo $circulo . "<br>"; // Uso de __toString
+
+$circulo->color = "amarillo"; // Modificación a través de __set
+echo "Color del círculo modificado: " . $circulo->color . "<br>";
+
 $figura = new Figura("rojo", 4);
 
 
